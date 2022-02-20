d40c37582f7e34039c176d8a88d2ac5c0984f260
Grigory Vasilev <grishavasilev62@gmail.com>
2022-02-20 22:53:55 +0300

    Передали управление формой презентору

--- Harvest/HarvestMainFrame/Presentor.cs
+++ Harvest/HarvestMainFrame/Presentor.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HarvestMainFrame.Interfaces;
 
 namespace HarvestMainFrame
 {
 	class Presentor
 	{
+		// Создаем внутренний объект интерфейса формы, чтобы можно было ей управлять из презентора.
+		IMainForm mainForm;
+
+		public Presentor(IMainForm mainForm)
+		{
+			this.mainForm = mainForm;
+		}
 	}
 }
