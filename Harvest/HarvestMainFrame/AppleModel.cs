febabae50adb352c5f9a0804546ce00fb9815628
Grigory Vasilev <grishavasilev62@gmail.com>
2022-02-20 23:24:27 +0300

    Добавили модель яблока

---
+++ Harvest/HarvestMainFrame/AppleModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarvestMainFrame
+{
+	class AppleModel
+	{
+		int _xAxesCoordinate;
+		int _yAxesCoordinate;
+		int _appleRadius;
+		int _fallingSpeed;
+
+		public AppleModel(
+			int xAxesCoordinate,
+			int yAxesCoordinate,
+			int appleRadius,
+			int fallingSpeed)
+		{
+			_xAxesCoordinate = xAxesCoordinate;
+			_yAxesCoordinate = yAxesCoordinate;
+			_appleRadius = appleRadius;
+			_fallingSpeed = fallingSpeed;
+		}
+	}
+}
